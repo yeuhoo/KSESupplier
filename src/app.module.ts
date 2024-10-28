3935ae8623e54070a40b848ab18bb3b08783c665
prince <prince.oncada@gmail.com>
2024-10-28 11:51:03 -0400

    dev: disabled default graphql playground and replaced with graphiql accessible via localhost:3000/graphiql/graphiql.html

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,23 @@ import { join } from 'path';
 import { AppService } from './app.service';
 import { AppResolver } from './app.resolver';
 import { ConfigModule } from '@nestjs/config';
-
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true, 
-    }),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-    }),
-  ],
-  providers: [AppService, AppResolver],
+    imports: [
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, '..', 'public'),
+            serveRoot: '/graphiql',                   
+        }),
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+            playground: false
+        }),
+    ],
+    providers: [AppService, AppResolver],
 })
-export class AppModule {}
+export class AppModule { }
