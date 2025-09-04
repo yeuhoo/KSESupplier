54351f5bb4185755295b8ae4fa50a0fae05c60c2
Chek <zlorenzo@mcm.edu.ph>
2025-09-04 14:36:55 +0800

    revert/delete

--- src/app.service.ts
+++ src/app.service.ts
@@ -2012,7 +2012,7 @@ export class AppService {
       return true;
     } catch (error) {
       console.error("Error sending draft order invoice:", error.message);
-      throw new Error("Failed to send draft order invoice. " + error.message);
+      throw new Error("Failed to send draft order invoice. 1" + error.message);
     }
   }
 }
